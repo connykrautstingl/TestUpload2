bb271cb0ddbe1c64b307e62ddb1bdcb6f80b9c6f
Connykrautstingl <s44784@edu.campus02.at>
2021-10-09 10:07:27 +0200

    2nd line added

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
 
     public static void main(String[] args) {
         System.out.println("Ich bin ein erneuter Test");
+        System.out.println("Ich bin die zweite Zeile zum Testen ");
     }
 }
